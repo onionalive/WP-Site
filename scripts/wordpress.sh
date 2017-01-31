c810d7112c5b52839c213936124ae02f5a580bc1
Stephen <stephenritchie0@gmail.com>
2017-01-31 14:04:17 +1100

    update script with salts

--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -16,12 +16,16 @@ echo "Done!"
 echo "*////////////////////////////////////////////////////////"
 echo " *               Wordpress Setup                        *"
 echo "*///////////////////////////////////////////////////////"
+echo ""
 echo "Let's get the information for the database to connect to"
+echo ""
 read -r -p "Database name: " database_name
 read -r -p "Database User: " database_user
 read -r -p "Database Password: " database_pw
 
+echo ""
 echo "Awesome, updating wp-config with this information."
+echo ""
 
 # update wp-config with  correct databse details
 sed -ie "s/database_name_here/$database_name/g" ../wp/wp-config.php
@@ -31,7 +35,21 @@ sed -ie "s/password_here/$database_pw/g" ../wp/wp-config.php
 # update wp-config to point to
 # correct directory
 sed -i "40i define( 'WP_CONTENT_DIR', dirname(dirname(__FILE__)) . '/wp-content' );" ../wp/wp-config.php
-sed -i "41i define( 'WP_CONTENT_URL', 'http://localhost/wp-$database_name/wp-content' )" ../wp/wp-config.php
+sed -i "41i define( 'WP_CONTENT_URL', 'http://localhost/wp-$database_name/wp-content' );" ../wp/wp-config.php
+echo "Done, grabbing salts from wordpress API and applying to config"
+echo ""
+
+#create hashes for wp-config
+#set WP salts
+SALTS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+while read -r SALT; do
+SEARCH="define('$(echo "$SALT" | cut -d "'" -f 2)"
+REPLACE=$(echo "$SALT" | cut -d "'" -f 4)
+echo "... $SEARCH ... $SEARCH ..."
+sed -i "/^$SEARCH/s/put your unique phrase here/$(echo $REPLACE | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/" ../wp/wp-config.php
+done <<< "$SALTS"
+
+echo ""
 echo "Done, wp-config should be updated."
 echo "Dont forget to turn on timber and switch to the correct theme in the WP admin"
 echo ":^)"
